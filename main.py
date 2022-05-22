3e8ad9e5ef2b49fec9b10698a51fa9f4062bb972
urszkam <ursz.kam@gmail.com>
2022-05-22 20:43:48 +0200

    Something

--- main.py
+++ main.py
@@ -4,17 +4,56 @@ class HerokuApp:
     app_url = "https://fierce-spire-87558.herokuapp.com/"
 
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
 @app.get("/")
 def root():
-    return {"start": "1970-01-01"}
-
+    return {"start": "1970-0101"}
 
 @app.post("/method", status_code=201)
-def root():
+def post():
     return {"method": "POST"}
+
+@app.get("/method", status_code=200)
+def get():
+    return {"method": "GET"}
+
+@app.delete("/method", status_code=200)
+def delete():
+    return {"method": "DELETE"}
+
+@app.put("/method", status_code=200)
+def put():
+    return {"method": "PUT"}
+
+@app.options("/method", status_code=200)
+def options():
+    return {"method": "OPTIONS"}
+
+# class EventDetails(BaseModel):
+#     date: str
+#     event: str
+
+# @app.put("/events", status_code=200)
+# async def add_event(item: EventDetails, request: Request):
+#     json_info = await request.json()
+#     id = counter()
+
+#     new_json = {
+#         "date" : json_info["date"],
+#         "name" : json_info["event"],
+#         "date_added" : datetime.date.today().strftime,
+#         "id" : id
+#     }
+
+    
+
+
+
+
+
 
 
 # app.get("/event/{date}",status_code=200)
