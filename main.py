077c07bf211fdf39dd364eef65041c4e6652d36f
urszkam <ursz.kam@gmail.com>
2022-05-30 23:46:46 +0200

    Something

--- main.py
+++ main.py
@@ -10,80 +10,104 @@ from collections import Counter
 
 
 app = FastAPI()
-app.counter = -1
-
-@app.get("/")
-def root():
-    return {"start": "1970-01-01"}
-
-@app.post("/method", status_code=201)
-def post():
-    return {"method": "POST"}
-
-@app.get("/method", status_code=200)
-def get():
-    return {"method": "GET"}
-
-@app.delete("/method", status_code=200)
-def delete():
-    return {"method": "DELETE"}
-
-@app.put("/method", status_code=200)
-def put():
-    return {"method": "PUT"}
-
-@app.options("/method", status_code=200)
-def options():
-    return {"method": "OPTIONS"}
-
-days = {1: "monday", 2: "tuesday", 3: "wednesday", 4: "thursday", 5: "friday", 6: "saturday", 7: "sunday"}
-
-@app.get("/day")
-def day(name: str, number: int, response: Response):
-    if number in days:
-        if days.get(number) == name:
-            return days[number]
-        else:
-            response.status_code = status.HTTP_400_BAD_REQUEST
-    else:
-        response.status_code = status.HTTP_400_BAD_REQUEST
 
 
-class Item(BaseModel):
-    date: str
-    event: str
 
-@app.put("/events", status_code=200)
-def add_event(item: Item):
-    app.counter += 1
-    global cal
-    global events
 
-    cal = {
-        "id" : app.counter,
-        "name" : item.event,
-        "date" : item.date,
-        "date_added" : datetime.date.today().strftime("%Y-%m-%d"),
-    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# app.counter = -1
+
+# @app.get("/")
+# def root():
+#     return {"start": "1970-01-01"}
+
+# @app.post("/method", status_code=201)
+# def post():
+#     return {"method": "POST"}
+
+# @app.get("/method", status_code=200)
+# def get():
+#     return {"method": "GET"}
+
+# @app.delete("/method", status_code=200)
+# def delete():
+#     return {"method": "DELETE"}
+
+# @app.put("/method", status_code=200)
+# def put():
+#     return {"method": "PUT"}
+
+# @app.options("/method", status_code=200)
+# def options():
+#     return {"method": "OPTIONS"}
+
+# days = {1: "monday", 2: "tuesday", 3: "wednesday", 4: "thursday", 5: "friday", 6: "saturday", 7: "sunday"}
+
+# @app.get("/day")
+# def day(name: str, number: int, response: Response):
+#     if number in days:
+#         if days.get(number) == name:
+#             return days[number]
+#         else:
+#             response.status_code = status.HTTP_400_BAD_REQUEST
+#     else:
+#         response.status_code = status.HTTP_400_BAD_REQUEST
+
+
+# class Item(BaseModel):
+#     date: str
+#     event: str
+
+# @app.put("/events", status_code=200)
+# def add_event(item: Item):
+#     app.counter += 1
+#     global cal
+#     global events
+
+#     cal = {
+#         "id" : app.counter,
+#         "name" : item.event,
+#         "date" : item.date,
+#         "date_added" : datetime.date.today().strftime("%Y-%m-%d"),
+#     }
     
-    events = []
-    events.append(cal)
+#     events = []
+#     events.append(cal)
 
-    return cal
+#     return cal
 
 
-@app.get("/events/{date}", status_code=200)
-def event_on_date(date: str, response: Response):
-    try:
-        datetime.datetime.strptime(date, "%Y-%m-%d")
-    except ValueError:
-        response.status_code = status.HTTP_400_BAD_REQUEST
-    else:
-        for e in events:
-            if date == e['date']:    
-                return events
-            else:
-                response.status_code = status.HTTP_404_NOT_FOUND
+# @app.get("/events/{date}", status_code=200)
+# def event_on_date(date: str, response: Response):
+#     try:
+#         datetime.datetime.strptime(date, "%Y-%m-%d")
+#     except ValueError:
+#         response.status_code = status.HTTP_400_BAD_REQUEST
+#     else:
+#         for e in events:
+#             if date == e['date']:    
+#                 return events
+#             else:
+#                 response.status_code = status.HTTP_404_NOT_FOUND
     
 # app.get("/events/{date}",status_code=200)
 # def event_date(date: str, response: Response):
--- tests/main.py
+++
@@ -1,2 +0,0 @@
-class HerokuApp:
-    app_url = "https://fierce-spire-87558.herokuapp.com"  # Fill your heroku app url here
